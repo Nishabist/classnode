07bdb6dd11e7b83ea2b8c89ac8bf120e93746355
Nisha <nishabist27@gmail.com>
2023-10-15 22:36:09 +0545

    express

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/users', (req, res) => {
+  res.send({name:"nisha",caste:"bist"})
 })
 
 app.listen(port, () => {
