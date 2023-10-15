f9978059326a23f2314263ef0c005fe59043ec77
Nisha <nishabist27@gmail.com>
2023-10-15 21:02:31 +0545

    getting started with nodejs

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const arr=[
-    {
-        name:'Nisha',
-        address:"Kanchanpur"
-    }
-]
-console.log(arr);
+const http = require('http');
+ 
+const hostname = 'local host';
+const port = 3000;
+ 
+const server = http.createServer((req, res) => {
+  res.end('Hello World');
+});
+ 
+server.listen(port);
+ 
