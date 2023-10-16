0f2cd74c24cb41e9f364d95d50cbe2acd7b80516
Nisha <nishabist27@gmail.com>
2023-10-16 15:57:33 +0545

    search by using filter/ query part

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const users =[
+    {id:1,name:"Nisha",address:"Kanchanour"},
+    {id:2,name:"kaylin", address:"Ktm"}
+]
+
 app.get('/users', (req, res) => {
-  res.send({name:"nisha",caste:"bist"})
+  const searchlist=users.filter((items,id)=>{
+    if(items.name[0]===req.query.startswith && items.name[items.name.length-1]===req.query.endswith){
+      return items
+    }
+  })
+  res.send(searchlist)
 })
 
 app.listen(port, () => {
